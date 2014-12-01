2f360d9986c13adaaf670b80b27dad995823b849
sharkdp <davidpeter@web.de>
2014-12-01 17:06:45 +0100

    Use new functions for getting (next) nearest neighbors

--- bandstructure/system/tightbindingsystem.py
+++ bandstructure/system/tightbindingsystem.py
@@ -12,17 +12,10 @@ class TightBindingSystem(System):
         t = self.get("t")
         t2 = self.get("t2")
 
-        # Nearest neighbors:
-
-        # Only with newest numpy version:
-        # nn = np.linalg.norm(dr, axis=3) == 1   # TODO! get the real nearest neighbor distance
-        # nnn = np.linalg.norm(dr, axis=3) == 2  # TODO!
-
-        nn = np.sqrt(np.sum(dr ** 2, axis=3)) == 1   # TODO! get the real nearest neighbor distance
-        nnn = np.sqrt(np.sum(dr ** 2, axis=3)) == 2  # TODO
-
         # Orbital matrix
         m = np.array([[1, 0], [0, -1]])
         # m = np.array([-t])
 
+        nn = dr.getNeighborsMask(1)
+        nnn = dr.getNeighborsMask(2)
         return t * m * nn[:, :, :, None, None] + t2 * m * nnn[:, :, :, None, None]
