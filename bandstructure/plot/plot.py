1120659379782ca6724086a1378c57e3ab567322
sharkdp <davidpeter@web.de>
2014-12-01 17:46:37 +0100

    3D plotting

--- bandstructure/plot/plot.py
+++ bandstructure/plot/plot.py
@@ -6,7 +6,10 @@ class Plot:
     def __init__(self, system):
         self.system = system
 
-    def plotDispersionPath(self, points=[[np.pi, np.pi], [0, 0], [np.pi, 0], [np.pi, np.pi]], resolution=300, processes=None):
+    def plotDispersionPath(self,
+                           points=[[np.pi, np.pi], [0, 0], [np.pi, 0], [np.pi, np.pi]],
+                           resolution=300,
+                           processes=None):
         """Plot the dispersion relation of the bands along a path in the Brillouin zone."""
 
         self.system.initialize()
@@ -17,7 +20,7 @@ class Plot:
         plt.plot(length, energies)
         plt.savefig('dispersion.pdf')
 
-    def plotDispersion(self, resolution=100, processes=None):
+    def plotDispersion(self, resolution=50, processes=None):
         """Plot the dispersion relation of the bands along a path in the Brillouin zone."""
 
         self.system.initialize()
@@ -25,7 +28,21 @@ class Plot:
         kvecs = self.system.lattice.getKvectorsZone(resolution)
         energies = self.system.solve(kvecs, processes)
 
-        plt.imshow(energies[:, :, 0], interpolation='nearest')
+        from mpl_toolkits.mplot3d import Axes3D
+        from matplotlib import cm
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        for band in range(energies.shape[-1]):
+            ax.plot_surface(kvecs[:, :, 0],
+                            kvecs[:, :, 1],
+                            energies[:, :, band],
+                            cstride=1,
+                            rstride=1,
+                            cmap=cm.coolwarm,
+                            linewidth=0.03,
+                            antialiased=False
+                            )
         plt.savefig('dispersion.pdf')
 
     def plotDispersion2D(self):
