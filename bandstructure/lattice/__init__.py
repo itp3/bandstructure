ff94586429e3a41c995ea6688598db12f1b2608b
sebanweb <sebastian.a.weber@gmail.com>
2014-12-08 16:29:57 +0100

    Kagome lattice added

--- bandstructure/lattice/__init__.py
+++ bandstructure/lattice/__init__.py
@@ -1,4 +1,5 @@
 from .lattice import Lattice
 from .squarelattice import SquareLattice
 from .honeycomblattice import HoneycombLattice
+from .kagomelattice import KagomeLattice
 from .regularchain import RegularChain
